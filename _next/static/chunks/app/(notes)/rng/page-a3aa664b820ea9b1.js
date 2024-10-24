9155ffd540620cd67000e91458467921893d5ff3
kolya <aktoaktov@yandex.ru>
2024-10-24 20:59:52 +0300

    a

--- _next/static/chunks/app/(notes)/rng/page-a3aa664b820ea9b1.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[73],{5692:function(n,e,t){Promise.resolve().then(t.t.bind(t,5412,23)),Promise.resolve().then(t.t.bind(t,6982,23))},5412:function(){},6982:function(){}},function(n){n.O(0,[507,88,971,117,744],function(){return n(n.s=5692)}),_N_E=n.O()}]);
